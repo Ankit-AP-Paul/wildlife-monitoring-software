0055d48f7e4b8d3ab534f9f89cc7e992175957cb
Ankit-AP-Paul <ankitpaul894@gmail.com>
2024-07-25 09:40:50 +0530

    added endpoint get region by id

--- backend/routes/region.py
+++ backend/routes/region.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from bson.objectid import ObjectId
 
 from models.region import Region
-from schemas.region import regionsEntity
+from schemas.region import regionsEntity, regionEntity
 from config.db import conn
 
 region = APIRouter()
@@ -11,7 +12,15 @@ async def create_region(region: Region):
     conn.technoverse.region.insert_one(dict(region))
     return region.dict()
 
-@region.get('/region')
-async def get_regions():
+@region.get('/all_region')
+async def get_all_regions():
     regions = conn.technoverse.region.find()
     return regionsEntity(regions)
+
+@region.get('/region/{id}')
+async def get_region_by_id(id):
+    id = ObjectId(id)
+    region = conn.technoverse.region.find_one({"_id": id})
+    if not region:
+        raise HTTPException(status_code=404, detail="Region Not Found")
+    return regionEntity(region)
